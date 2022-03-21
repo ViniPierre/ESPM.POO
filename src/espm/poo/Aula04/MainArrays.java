b7375841d91269cfb8e5df9e0668ca2a75e0e785
Diego Calciolari Gordiano <81247672+ViniPierre@users.noreply.github.com>
2022-03-21 10:35:21 -0300

    atualizando o arquivo

--- src/espm/poo/Aula04/MainArrays.java
+++ src/espm/poo/Aula04/MainArrays.java
@@ -3,26 +3,31 @@ package src.espm.poo.Aula04;
 import java.util.Arrays;
 
 public class MainArrays {
-    
-    public static void main(String[] args) {
-    int[] ns = new int[10];
 
-        for (int n : ns) {
+    public static void main(String[] args) {
+        int[] ns = new int[10];
+
+        for (int n: ns) {
             System.out.println(n);
         }
 
         for (int i = 0; i < ns.length; i++) {
             System.out.println(ns[i]);
-
         }
 
         Arrays.asList(ns).forEach(n -> System.out.println(n));
 
-        int[][] matriz new int int[][] {
-            {1 ,2 ,3}
-            {4 ,5 ,6}
-            {7 ,8 ,9}
+        int[][] matriz = new int[][] {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        for (int[] l: matriz) {
+            for (int c: l) {
+                System.out.println(c);
+            }
         }
     }
-
+    
 }
