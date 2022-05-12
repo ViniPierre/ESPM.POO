28a138392b1ec53a868772bb9951891d7cd7a2bd
Diego Calciolari Gordiano <81247672+ViniPierre@users.noreply.github.com>
2022-05-12 10:02:21 -0300

    mudança

--- src/espm/poo/aula9/MainCalc.java
+++ src/espm/poo/aula9/MainCalc.java
@@ -5,86 +5,86 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class MainCalc {
-    
+
     public static void main(String[] args) {
-        Scanner teclado = new Scanner(System.in);
-        
-while (true) {
-    try {
-        System.out.print("Número 1: ");
-        double a = teclado.nextDouble();
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Número 2: ");
-        double b = teclado.nextDouble();
-
-        String op = null;
-        while (op == null) {
+        while (true) {
             try {
-                op = input("operações [+ - / * ^ !]: ", "-","+","/","!","*","^");
-            } catch (ESPMException e) {
-                System.err.println("Entrada inválida " + e.getMessage());
-                throw new RuntimeException(e.getMessage(), e);
-            }
-        }
-
-        if (op.equals("!")) {
-            break;
-        }
-        double x = op.equals("-") ? sub(a, b) :
-                op.equals("+") ? add(a, b) :
-                op.equals("*") ? mult(a, b) :
-                op.equals("/") ? div ((int) a, (int) b) :
-                op.equals("^") ? exp(a, b) :
-                0;
+                System.out.print("a: ");
+                double a = scanner.nextDouble();
+                System.out.print("b: ");
+                double b = scanner.nextDouble();
+                scanner.nextLine();
+                String op = null;
+                while (op == null) {
+                    try {
+                        op = input("operacao [+-/*^!]: ", "*", "/", "!", "+", "-");
+                    } catch(ESPMException e) {
+                        System.err.println("Entrada inválida: " + e.getMessage());
+                        throw new RuntimeException(e.getMessage(), e);
+                        // continua propagando a excecao
+                        // throw e;
+                    }
+                }
+                if (op.equals("!")) {
+                    break;
+                }
+                double x = op.equals("-") ? sub(a, b) :
+                        op.equals("+") ? add(a, b) :
+                        op.equals("*") ? mul(a, b) :
+                        op.equals("/") ? div((int) a, (int) b) :
+                        op.equals("^") ? exp(a, b) :
+                        0;
 
                 System.out.println(a + op + b + " = " + x);
             } catch (ArithmeticException e) {
                 e.printStackTrace();
-                System.err.println("Erro na operação: " + e.getMessage());
+                System.err.println("Erro na operacao: " + e.getMessage());
             } catch (InputMismatchException e) {
                 e.printStackTrace();
-                System.err.println("Erro na operação: " + e.getMessage());
-                teclado.nextLine();
+                System.err.println("Erro na entrada: " + e.getMessage());
+                scanner.nextLine();
             } catch (Exception e) {
                 e.printStackTrace();
                 System.err.println("Erro: " + e.getMessage());
             } finally {
-                System.out.println("Operação ok ou exceção tratada!");
+                System.out.println("Operacao ok ou excecao tratada!");
             }
         }
     }
-    
+
     private static String input(String msg, String... possibles) throws ESPMException {
-        final Scanner teclado = new Scanner(System.in);
+        final Scanner scanner = new Scanner(System.in);
         System.out.print(msg);
-        String line = teclado.nextLine().trim();
+        String line = scanner.nextLine().trim();
         for (String item : Arrays.asList(possibles)) {
             if (item.equals(line)) {
                 return item;
             }
         }
+        // Levanta uma excecao
         throw new ESPMException(line);
-    }
-
-    private static double exp(double a, double b) {
-        return Math.pow(a, b);
-    }
-
-    private static double div(double a, double b) {
-        return a / b;
-    }
-
-    private static double mult(double a, double b) {
-        return a * b;
-    }
-
-    private static double add(double a, double b) {
-        return a + b;
     }
 
     private static double sub(double a, double b) {
         return a - b;
     }
 
-    
+    private static double add(double a, double b) {
+        return a + b;
+    }
+
+    private static double mul(double a, double b) {
+        return a * b;
+    }
+
+    private static double div(int a, int b) {
+        return a / b;
+    }
+
+    private static double exp(double a, double b) {
+        return Math.pow(a, b);
+    }
+
 }
